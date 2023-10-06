d69a38efe1290a8fbc30737e2fa796b775170f8f
abdullahi samsudeen <abdullahisamsudeen@gmail.com>
2023-10-06 15:47:20 +0100

    commit

--- src/component/Dashboard.jsx
+++ src/component/Dashboard.jsx
@@ -7,28 +7,30 @@ import { Link } from 'react-router-dom'
 
 
 const Dashboard = () => {
-    const [userData, setUserData] = useState("")
+  const [userData, setUserData] = useState("")
 
-    useEffect(() => {
-        let userDetail =JSON.parse(localStorage.getItem('user')) 
-        console.log(userDetail);
-        console.log(userDetail.userName);
-        setUserData(userDetail.userName)
-      
-    }, [])
-    
+  useEffect(() => {
+    let userDetail = JSON.parse(localStorage.getItem('user'))
+    console.log(userDetail);
+    console.log(userDetail.userName);
+    setUserData(userDetail.userName)
+
+  }, [])
+
   return (
     <>
-    <div className='background'>
-    <div className='absolute bg-blue-950 lg:w-[30%] mt-48 lg:ms-[480px] ms-[20px]  w-[90%] rounded px-2'>
+      <div className='background'>
+        <div className='absolute bg-blue-950 lg:w-[30%] mt-48 lg:ms-[480px] ms-[20px]  w-[90%] rounded px-2'>
           <h1 className='text-4xl text-center my-3 text-white font-bold font-serif'>Hi, {userData}</h1>
-          <a className='text-red-500 my-2 hover:text-fuchsia-500 lg:ms-16 font-serif'>https://beautiful-taffy-79e1d2.netlify.app/message/{userData}</a><br />
+          <div className='text-center'>
+            <a className='text-red-500 my-2 hover:text-fuchsia-500   font-serif'>https://beautiful-taffy-79e1d2.netlify.app/message/{userData}</a><br />
+          </div>
           <p className='text-white font-serif'>Copy your profile link ❤ to get responses from your friend. to check out responses you can go to "View Messages". 🍟😃 </p>
           <div id='view' className='my-5 h-10 text-center items-center rounded-lg'>
             <Link className='hover:text-blue-950 text-xl font-serif text-white' to="/View">View Messages</Link>
           </div>
-    </div>
-  </div>
+        </div>
+      </div>
     </>
   )
 }
