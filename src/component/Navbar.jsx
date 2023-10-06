13fdd175c804e23b643769613dcb96301a673f33
abdullahi samsudeen <abdullahisamsudeen@gmail.com>
2023-10-06 15:37:01 +0100

    commit

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import '../App.css'
+import {BiLogoKickstarter} from 'react-icons/bi'
 
 
 
@@ -13,7 +14,7 @@ const Navbar = () => {
     <nav className="bg-blue-950">
     <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
       <a href="" className="flex items-center">
-          <img src="https://flowbite.com/docs/images/logo.svg" className="h-8 mr-3" alt="Flowbite Logo" />
+          <BiLogoKickstarter size={30} className="text-white" alt="Flowbite Logo" />
           <span className="self-center text-2xl font-semibold whitespace-nowrap text-white">Flowbite</span>
       </a>
       <button data-collapse-toggle="navbar-default" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">
