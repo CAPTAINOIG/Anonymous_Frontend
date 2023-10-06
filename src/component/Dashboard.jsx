fe3e29c0b8c3ba31274f31bc2f97c5c19c64e68b
abdullahi samsudeen <abdullahisamsudeen@gmail.com>
2023-10-06 16:53:15 +0100

    commit

--- src/component/Dashboard.jsx
+++ src/component/Dashboard.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import copy from 'clipboard-copy'; // Import clipboard-copy library
-
+import {MdContentCopy} from 'react-icons/md'
 const Dashboard = () => {
   const [userData, setUserData] = useState('');
   const avatarImageUrl = 'https://example.com/avatar.jpg'; // Replace with your avatar URL
@@ -30,9 +30,9 @@ const Dashboard = () => {
       <div className='background'>
         <div className='absolute bg-blue-950 lg:w-[30%] mt-48 lg:ms-[480px] ms-[20px]  w-[90%] rounded px-2'>
           <h1 className='text-4xl text-center my-3 text-white font-bold font-serif'>Hi, {userData}</h1>
-          <div className='text-center'>
-            <a className='text-red-500 my-2 hover:text-fuchsia-500  font-serif'>{`https://beautiful-taffy-79e1d2.netlify.app/message/${userData}`}</a><br />
-            <button onClick={handleCopyAvatarClick} className='text-white my-2 hover:text-blue-500  font-serif'>Copy Avatar</button>
+          <div className='text-center flex'>
+            <a className='text-red-500 my-2 hover:text-fuchsia-500  font-serif'>{`https://beautiful-taffy-79e1d2.netlify.app/message/${userData}`}</a>
+            <button onClick={handleCopyAvatarClick} className='text-white my-2 hover:text-blue-500  font-serif'><span><MdContentCopy size={20}/></span></button>
           </div>
           <p className='text-white font-serif'>Copy your profile link ❤ to get responses from your friend. to check out responses you can go to "View Messages". 🍟😃 </p>
           <div id='view' className='my-5 h-10 text-center items-center rounded-lg'>
